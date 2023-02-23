cc5f5c2045ee23c5a99e579b72d953804a21eeb9
Annson <annson@me.com>
2023-02-23 13:04:10 -0600

    Move W01D01 files

---
+++ W2D03-Intro-To-JS/loops.js
@@ -0,0 +1,52 @@
+// For Loops
+
+// Operators
+// ++ - increments by one
+// -- - decrements by one
+// != - not equal
+// % - modulo (remainder operator)
+// + - add
+// - - subtract
+// * - multiply
+// / - divide
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// what is the value of i outside of the loop
+// console.log(i);
+
+// Exercises:
+
+// 1.
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+// 2.
+// for (i = 1; i < 10; i += 2) {
+//   console.log(i);
+// }
+
+// for (i = 2; i <= 10; i += 2) {
+//   console.log(i);
+// }
+
+// 3.
+// for (i = 6; i <= 60; i += 3) {
+//   console.log(i);
+// }
+
+// for (let i = 6; i <= 60; i++) {
+//   if (i % 3 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 4.
+let x = "#";
+for (i = 1; i <= 7; i++) {
+  console.log(x);
+  x += "#";
+}
