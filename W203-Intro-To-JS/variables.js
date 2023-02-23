a058975e06b5507c781f6121351952efc2d33289
Annson <annson@me.com>
2023-02-23 10:46:25 -0600

    add js files

---
+++ W203-Intro-To-JS/variables.js
@@ -0,0 +1,26 @@
+// Variables
+// What we store our data in, in JS
+
+// 4 components of a variable
+// 1. identifier
+  // the identifier tells JS what kind of variable it is
+  // let - variable can be reassigned
+  // const - variable is constant & can't be reassigned
+// 2. name
+  // camelCase
+  // snake_case
+  // kebab-case
+// 3. the assignment operator =
+// 4. the value
+  // string: ""
+  // number: 9
+  // boolean: true/false
+  // arrays []
+  // objects: {}
+  // null
+  // undefined
+
+
+// Exercise:
+
+let age = 20
