1a2014019e564befd115c1b607f8566593f5e688
Annson <annson@me.com>
2023-03-07 11:24:24 -0600

    add makeSentence function

--- W04/D02/lab.js
+++ W04/D02/lab.js
@@ -6,3 +6,9 @@ function minusOne(num) {
   }
 }
 // console.log(minusOne("infinity"));
+
+function makeSentence(s1, s2, s3) {
+  console.log(`Hello ${s1}, here are the ${s2} ${s3} that you ordered.`);
+}
+
+// makeSentence("Jenny", "blue", "pants");
