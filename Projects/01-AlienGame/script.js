aa80934e0205ebb962009a471be7ea15099cf536
Annson <annson@me.com>
2023-03-16 12:57:57 -0500

    create ship

--- Projects/01-AlienGame/script.js
+++ Projects/01-AlienGame/script.js
@@ -1,7 +1,18 @@
+const ship = {
+  name: "USS Assembly",
+  hull: 20,
+  firepower: 5,
+  accuracy: 0.7,
+};
+
 class Alien {
   constructor() {
+    this.name = `Alien${Alien.num++}`;
     this.hull = Math.floor(Math.random() * 4) + 3;
     this.firepower = Math.floor(Math.random() * 3) + 2;
     this.accuracy = Math.random() * 0.2 + 0.6;
   }
+  static num = 1;
 }
+// console.log(new Alien());
+// console.log(new Alien());
