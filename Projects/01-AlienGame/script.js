556cf4babab54ae47185d8ce8b89a6ec80daeff2
Annson <annson@me.com>
2023-03-16 17:46:44 -0500

    add missile functionality

--- Projects/01-AlienGame/script.js
+++ Projects/01-AlienGame/script.js
@@ -3,6 +3,7 @@ const ship = {
   hull: 20,
   firepower: 5,
   accuracy: 0.7,
+  missiles: 3,
 };
 
 const alienFleet = {
@@ -23,6 +24,7 @@ class Alien {
   static num = 1;
 }
 
+// random # of attackers generator
 const attackers = Math.floor(Math.random() * 5) + 4;
 for (let i = 1; i <= attackers; i++) {
   alienFleet.create();
@@ -32,8 +34,8 @@ console.log(
   `There are ${attackers} alien ships attacking Earth! Take them down!`
 );
 
-// console.log(alienFleet);
 while (ship.hull > 0) {
+  // targeting functionality
   let str = `There are ${alienFleet.aliens.length} alien ships left. Pick a ship number to attack:
 `;
   for (let i = 0; i < alienFleet.aliens.length; i++) {
@@ -52,7 +54,36 @@ while (ship.hull > 0) {
   } else {
     target = alienFleet.aliens[targetNum - 1];
   }
-  // ship attack
+
+  // missile functionality
+  if (ship.missiles > 0) {
+    const fireMiss = window.prompt(
+      `You have ${ship.missiles} missiles. Would you like to fire one? Y/N?`
+    );
+    if (fireMiss.toLowerCase() == "yes" || fireMiss.toLowerCase() == "y") {
+      ship.missiles--;
+      console.log(
+        `Missile fired. ${target.name} destroyed. You have ${ship.missiles} missiles left.`
+      );
+      alienFleet.aliens.splice(targetNum - 1, 1);
+      if (!alienFleet.aliens[0]) {
+        console.log(
+          `The entire alien fleet has been destroyed. Earth is saved!`
+        );
+        break;
+      }
+      continue;
+    } else if (
+      fireMiss.toLowerCase() == "no" ||
+      fireMiss.toLowerCase() == "n"
+    ) {
+    } else {
+      console.log(`Invalid response. Shutting ship down.`);
+      break;
+    }
+  }
+
+  // ship attack turn
   console.log(`You fire your lasers.`);
   if (ship.accuracy >= Math.random()) {
     target.hull -= ship.firepower;
@@ -85,6 +116,8 @@ while (ship.hull > 0) {
   } else {
     console.log(`You missed!`);
   }
+
+  // shield functionality
   const shield = Math.floor(Math.random() * 3 + 1);
   console.log(
     `Your shields fluctuate and provide ${shield} hull points. You now have ${
@@ -92,7 +125,8 @@ while (ship.hull > 0) {
     } hull points.`
   );
   let attacker = alienFleet.aliens[0];
-  // alien attack
+
+  // alien attack turn
   console.log(`${attacker.name} fires its lasers.`);
   if (attacker.accuracy >= Math.random()) {
     ship.hull -= attacker.firepower - shield;
