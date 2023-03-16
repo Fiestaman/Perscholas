8c217aa04ebb0fd1c9962bb16fc7679010655e8f
Annson <annson@me.com>
2023-03-16 17:10:11 -0500

    add targeting functionality

--- Projects/01-AlienGame/script.js
+++ Projects/01-AlienGame/script.js
@@ -34,14 +34,31 @@ console.log(
 
 // console.log(alienFleet);
 while (ship.hull > 0) {
-  let target = alienFleet.aliens[0];
+  let str = `There are ${alienFleet.aliens.length} alien ships left. Pick a ship number to attack:
+`;
+  for (let i = 0; i < alienFleet.aliens.length; i++) {
+    str = str + (i + 1) + `. ` + alienFleet.aliens[i].name + ` `;
+    if (i == 6) {
+      str =
+        `${str}` +
+        `
+`;
+    }
+  }
+  let targetNum = window.prompt(str);
+  if (targetNum < 1 || targetNum > alienFleet.aliens.length) {
+    console.log(`Invalid target. Shutting ship down.`);
+    break;
+  } else {
+    target = alienFleet.aliens[targetNum - 1];
+  }
   // ship attack
   console.log(`You fire your lasers.`);
   if (ship.accuracy >= Math.random()) {
     target.hull -= ship.firepower;
     if (target.hull <= 0) {
       console.log(`${target.name} was hit and has been destroyed.`);
-      alienFleet.aliens.shift();
+      alienFleet.aliens.splice(targetNum - 1, 1);
       if (!alienFleet.aliens[0]) {
         console.log(
           `The entire alien fleet has been destroyed. Earth is saved!`
@@ -49,9 +66,9 @@ while (ship.hull > 0) {
         break;
       }
       const prompt = window.prompt(
-        "Enter retreat to retreat or attack to attack the next ship."
+        "Enter retreat to retreat or attack to attack another ship."
       );
-      if (prompt == "retreat") {
+      if (prompt.toLowerCase() == "retreat") {
         console.log("You retreat to fight another day.");
         break;
       } else if (prompt.toLowerCase() == "attack") {
@@ -69,16 +86,17 @@ while (ship.hull > 0) {
     console.log(`You missed!`);
   }
 
+  let attacker = alienFleet.aliens[0];
   // alien attack
-  console.log(`${target.name} fires its lasers.`);
-  if (ship.accuracy >= Math.random()) {
-    ship.hull -= target.firepower;
+  console.log(`${attacker.name} fires its lasers.`);
+  if (attacker.accuracy >= Math.random()) {
+    ship.hull -= attacker.firepower;
     if (ship.hull <= 0) {
       console.log(`Your ship has been destroyed. Earth is doomed! You lose.`);
     } else if (ship.hull > 0) {
       console.log(`You have been hit and have ${ship.hull} hull remaining!`);
     }
   } else {
-    console.log(`${target.name} missed!`);
+    console.log(`${attacker.name} missed!`);
   }
 }
