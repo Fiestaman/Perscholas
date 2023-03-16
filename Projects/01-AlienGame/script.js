4aeb26e0baeb7f3fe6c12727b85e7c9a50bd9c7c
Annson <annson@me.com>
2023-03-16 12:49:59 -0500

    create Alien class

---
+++ Projects/01-AlienGame/script.js
@@ -0,0 +1,7 @@
+class Alien {
+  constructor() {
+    this.hull = Math.floor(Math.random() * 4) + 3;
+    this.firepower = Math.floor(Math.random() * 3) + 2;
+    this.accuracy = Math.random() * 0.2 + 0.6;
+  }
+}
