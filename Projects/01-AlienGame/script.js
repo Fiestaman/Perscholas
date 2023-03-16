46c1c1c8f9a2332beb56878811b0c6649dfbf72c
Annson <annson@me.com>
2023-03-16 15:29:51 -0500

    add loop to loop through all ships until complete

--- Projects/01-AlienGame/script.js
+++ Projects/01-AlienGame/script.js
@@ -28,42 +28,52 @@ for (let i = 1; i <= 6; i++) {
 }
 
 // console.log(alienFleet);
-
-let target = alienFleet.aliens[0];
-// ship attack
-if (ship.accuracy >= Math.random()) {
-  target.hull -= ship.firepower;
-  if (target.hull <= 0) {
-    console.log(`${target.name} has been destroyed.`);
-    const prompt = window.prompt(
-      "Enter retreat to retreat or attack to attack the next ship."
-    );
-    if (prompt.toLowerCase == "retreat") {
-      console.log("You retreat to fight another day.");
-      return;
-    } else if (prompt.toLowerCase == "attack") {
-      // continue loop to start attack cycle over
-    } else {
-      console.log("Invalid response. Shutting down ship.");
-      return;
+while (ship.hull > 0) {
+  let target = alienFleet.aliens[0];
+  // ship attack
+  console.log(`You fire your lasers.`);
+  if (ship.accuracy >= Math.random()) {
+    target.hull -= ship.firepower;
+    if (target.hull <= 0) {
+      console.log(`${target.name} was hit and has been destroyed.`);
+      alienFleet.aliens.shift();
+      if (!alienFleet.aliens[0]) {
+        console.log(
+          `The entire alien fleet has been destroyed. Earth is saved!`
+        );
+        break;
+      }
+      const prompt = window.prompt(
+        "Enter retreat to retreat or attack to attack the next ship."
+      );
+      if (prompt == "retreat") {
+        console.log("You retreat to fight another day.");
+        break;
+      } else if (prompt.toLowerCase() == "attack") {
+        continue;
+      } else {
+        console.log("Invalid response. Shutting down ship.");
+        break;
+      }
+    } else if (target.hull > 0) {
+      console.log(
+        `${target.name} has been hit and has ${target.hull} hull remaining!`
+      );
     }
-  } else if (target.hull > 0) {
-    console.log(
-      `${target.name} has been hit and has ${target.hull} hull remaining!`
-    );
+  } else {
+    console.log(`You missed!`);
   }
-} else {
-  console.log(`You missed!`);
-}
 
-// alien attack
-if (ship.accuracy >= Math.random()) {
-  ship.hull -= target.firepower;
-  if (ship.hull <= 0) {
-    console.log(`Your ship has been destroyed. Earth is doomed! You lose.`);
-  } else if (ship.hull > 0) {
-    console.log(`You have been hit and have ${ship.hull} hull remaining!`);
+  // alien attack
+  console.log(`${target.name} fires its lasers.`);
+  if (ship.accuracy >= Math.random()) {
+    ship.hull -= target.firepower;
+    if (ship.hull <= 0) {
+      console.log(`Your ship has been destroyed. Earth is doomed! You lose.`);
+    } else if (ship.hull > 0) {
+      console.log(`You have been hit and have ${ship.hull} hull remaining!`);
+    }
+  } else {
+    console.log(`${target.name} missed!`);
   }
-} else {
-  console.log(`${target.name} missed!`);
 }
