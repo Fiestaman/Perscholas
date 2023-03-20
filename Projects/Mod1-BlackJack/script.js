c89c5593ad401decc9fca4d57b34daf23ed02783
Annson <annson@me.com>
2023-03-20 11:59:19 -0500

    add deal a card function

--- Projects/Mod1-BlackJack/script.js
+++ Projects/Mod1-BlackJack/script.js
@@ -3,23 +3,30 @@ let pH = [];
 let dH = [];
 
 // random card picker
-function pick() {
+function pickCard() {
   return Math.floor(Math.random() * 13) + 1;
 }
 
 // deal hand function
-function deal() {
+function dealHand() {
   for (i = 1; i <= 2; i++) {
-    pH.push(pick());
-    dH.push(pick());
+    pH.push(pickCard());
+    dH.push(pickCard());
   }
 }
 
+// compare hand values to determine winner
 function scoreHand() {
-  let pS = pH[0] + pH[1];
-  let dS = dH[0] + dH[1];
+  let pS = 0;
+  for (let i = 0; i < pH.length; i++) {
+    pS += pH[i] > 10 ? 10 : pH[i];
+  }
+  let dS = 0;
+  for (let i = 0; i < dH.length; i++) {
+    dS += dH[i] > 10 ? 10 : dH[i];
+  }
   if (dS > pS) {
-    console.log(`Dealer had ${dS} while you ${pS}. You lose.`);
+    console.log(`Dealer had ${dS} while you had ${pS}. You lose.`);
   } else if (pS > dS) {
     console.log(`You had ${pS} while dealer had ${dS}. You win!`);
   } else if (pS == dS) {
@@ -27,6 +34,12 @@ function scoreHand() {
   }
 }
 
-deal();
+// deal a card function
+function dealCard(player) {
+  player.push(pickCard());
+}
+
+dealHand();
+dealCard(pH);
 console.log(pH, dH);
 scoreHand();
