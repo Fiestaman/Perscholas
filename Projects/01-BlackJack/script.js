661c3cf5fec3113a22b82527d5a4f7097a1c1be8
Annson <annson@me.com>
2023-03-21 11:18:24 -0500

    add split and double down function

--- Projects/01-BlackJack/script.js
+++ Projects/01-BlackJack/script.js
@@ -4,12 +4,17 @@ let p = {
   bet: 1,
   maxBal: 100,
   hand: [],
+  splitHand: [],
   pS: 0,
+  sHS: 0,
+  handBet: 1,
+  splitHB: 1,
 };
 let d = {
   hand: [],
   dS: 0,
 };
+let highScore = 100;
 
 // random card picker
 function pickCard() {
@@ -28,12 +33,15 @@ function dealHand() {
 function scoreHand() {
   if (d.dS > 21) {
     console.log(`Dealer busted. You win.`);
+    p.balance += p.handBet;
   } else if (d.dS > p.pS) {
     console.log(`Dealer had ${d.dS} while you had ${p.pS}. You lose.`);
   } else if (p.pS > d.dS) {
     console.log(`You had ${p.pS} while dealer had ${d.dS}. You win!`);
+    p.balance += p.handBet * 2;
   } else if (p.pS == d.dS) {
     console.log(`You push with the dealer. Bet refunded.`);
+    p.balance += p.handBet;
   }
 }
 
@@ -42,21 +50,21 @@ function dealCard(player) {
   player.hand.push(pickCard());
 }
 
-// increase bet function
-function increase(amt = 1) {
-  if (bet + amt > balance) {
-    `You do not have enough funds to bet that much.`;
-  }
-  bet += amt;
-}
+// // increase bet function
+// function increase(amt = 1) {
+//   if (p.bet + amt > p.balance) {
+//     `You do not have enough funds to bet that much.`;
+//   }
+//   p.bet += amt;
+// }
 
-// decrease bet function
-function decrease(amt = 1) {
-  if (bet + amt > balance) {
-    `You do not have enough funds to bet that much.`;
-  }
-  bet -= amt;
-}
+// // decrease bet function
+// function decrease(amt = 1) {
+//   if (p.bet + amt > p.balance) {
+//     `You do not have enough funds to bet that much.`;
+//   }
+//   p.bet -= amt;
+// }
 
 // reset balance function
 function reset() {
@@ -65,7 +73,8 @@ function reset() {
 
 // losing parameter
 if (p.balance <= 0) {
-  console.log(`You're bankrupt. Your highest balance was: ${maxBal}`);
+  console.log(`You're bankrupt. Your highest balance was: ${p.maxBal}`);
+  console.log(``);
 }
 
 // player hit function
@@ -73,10 +82,33 @@ function hit() {
   dealCard(p);
 }
 
+// split function
+function split() {
+  p.splitHand = [];
+  p.sHS = 0;
+  p.splitHand.push(p.hand.pop());
+  p.splitHB = p.handBet;
+}
+
+// double down function
+function double() {
+  p.bet *= 2;
+  dealCard(p);
+}
+
+// dealer deal own hand function
+function dealerDeal() {
+  while (d.dS < 17) {
+    dealCard(d);
+    d.dS += d.hand[d.hand.length - 1];
+  }
+}
+
 // play round function
 function playRound() {
   p.pS = 0;
   d.dS = 0;
+  p.handBet = p.bet;
   dealHand();
   for (let i = 0; i < p.hand.length; i++) {
     p.pS += p.hand[i] > 10 ? 10 : p.hand[i];
@@ -86,14 +118,19 @@ function playRound() {
   }
   // if clicked element is hit
   // hit()
-  // else if clicked element is stand
-  while (d.dS < 17) {
-    dealCard(d);
-    d.dS += d.hand[d.hand.length - 1];
-  }
+  // else if split
+
+  // else if double down
+  double();
+  // else clicked element is stand
 
   scoreHand();
   console.log(p.hand, d.hand);
 }
 
-playRound();
+document.querySelector("#set").addEventListener("click", function () {
+  p.bet = document.querySelector("#betAmt").value;
+  console.log(p.bet, p.handBet);
+});
+
+// playRound();
