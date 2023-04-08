5fd1742e9ac6c319c07a400b598ea6740794f30a
Annson <annson@me.com>
2023-04-08 10:31:49 -0500

    add W08D03 cryptoprices notes app

---
+++ W08/D03/cryptoprices/src/pages/Price.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export default function Price(props) {
+  // Our api key from coinapi.io
+  const apiKey = process.env.REACT_APP_API_KEY;
+  // Grabbing the Currency symbol from the URL Params
+  const { symbol } = useParams();
+  // Using the other two variables to create our URL
+  const url = `https://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
+  //state to hold the coin data
+  const [coin, setCoin] = useState("null");
+
+  const [refresh, setRefresh] = useState(false);
+
+  //function to fetch coin data
+  const getCoin = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCoin(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  // useEffect to run getCoin when component mounts
+  useEffect(() => {
+    getCoin();
+  }, [refresh]);
+
+  const handleClick = () => {
+    setRefresh(!refresh);
+  };
+
+  // loaded function for when data is fetched
+  const loaded = () => {
+    return (
+      <div>
+        <h1>
+          {coin.asset_id_base}/{coin.asset_id_quote}
+        </h1>
+        <h2>{coin.rate}</h2>
+        <button onClick={handleClick}>Refresh</button>
+      </div>
+    );
+  };
+  // Function for when data doesn't exist
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+  // if coin has data, run the loaded function, otherwise, run loading
+  return coin && coin.rate ? loaded() : loading();
+}
