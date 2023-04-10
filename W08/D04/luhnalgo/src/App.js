74d55d5435426b3100726561b1b8876963fdb9f4
Annson <21959423+Fiestaman@users.noreply.github.com>
2023-04-10 18:54:39 -0500

    add W08D04 homework

---
+++ W08/D04/luhnalgo/src/App.js
@@ -0,0 +1,45 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [card, setCard] = useState(null);
+
+  const validCard = (num) => {
+    if (!num) return;
+    const arr = num.toString().split("");
+    for (let i = arr.length - 2; i >= 0; i -= 2) {
+      arr[i] = arr[i] * 2;
+      if (arr[i] >= 10) {
+        arr[i] = arr[i] - 9;
+      }
+    }
+    const sum = arr.reduce((k, m) => {
+      return +k + +m;
+    });
+    return sum % 10 === 0;
+  };
+
+  // validCard(1234567890123456); //should be false
+  // validCard(4408041234567893); //should be true
+  // validCard(38520000023237); //should be true
+  // validCard(4222222222222); //should be true
+
+  return (
+    <div className="App">
+      <h1>Luhn Algorithm Validator</h1>
+      <input
+        type="text"
+        onKeyUp={(e) => {
+          setCard(e.target.value);
+        }}
+      />
+      <div>
+        {!card
+          ? "Please enter a card number"
+          : !validCard(card)
+          ? "Card is not valid"
+          : "Card is valid"}
+      </div>
+    </div>
+  );
+}
